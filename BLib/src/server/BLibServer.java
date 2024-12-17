5d028de6db1ba8625a63ac88ddfe216fc3bd0615
peleg <pelegc49@gmail.com>
2024-12-17 15:52:03 +0200

    updated

--- BLib/src/server/BLibServer.java
+++ BLib/src/server/BLibServer.java
@@ -1,12 +1,14 @@
 package server;
 
+import java.io.IOException;
+import java.util.List;
+
 import logic.Message;
 import ocsf.server.AbstractServer;
 import ocsf.server.ConnectionToClient;
 
-public class BLibServer extends AbstractServer{
+public class BLibServer extends AbstractServer {
 
-	
 	public BLibServer(int port) {
 		super(port);
 		// TODO Auto-generated constructor stub
@@ -14,20 +16,25 @@ public class BLibServer extends AbstractServer{
 
 	@Override
 	protected void handleMessageFromClient(Object msg, ConnectionToClient client) {
-		if(msg instanceof Message) {
-			switch(((Message) msg).getCommand().toLowerCase()) {
-			case "login":
-				
-			
-			
-			
+		if (msg instanceof Message) {
+			List<Object> args = ((Message) msg).getArguments();
+			String ret;
+			try {
+				switch (((Message) msg).getCommand().toLowerCase()) {
+				case "login":
+					ret = BLibDBC.login((String) args.get(0), (String) args.get(1));
+					if (ret != null) {
+						client.sendToClient(new Message("loginSuccess", ret));
+					}
+
+				}
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
-			
-			
+
 		}
-		
+
 	}
-	
-	
 
 }
