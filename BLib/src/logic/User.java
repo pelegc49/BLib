646e49b982ccd469174ccd4bd6b9b6d0b23b9ec2
lidorbd13 <Lidor.Ben.David@e.braude.ac.il>
2024-12-17 14:57:05 +0200

    added user

---
+++ BLib/src/logic/User.java
@@ -0,0 +1,38 @@
+package logic;
+
+public class User {
+
+	private String userName;
+	private String password;
+	private String type;
+	
+	public User(String userName, String password, String type) {
+        this.userName = userName;
+        this.password = password;
+        this.type = type;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
