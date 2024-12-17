5f8549e394c271b2c64f0fe0f238e2aec45dfdbb
lidorbd13 <Lidor.Ben.David@e.braude.ac.il>
2024-12-17 14:54:10 +0200

    added activity

---
+++ BLib/src/common/Activity.java
@@ -0,0 +1,50 @@
+package common;
+
+import java.util.Date;
+
+public class Activity {
+
+	private int id;
+	private String type;
+	private String description;
+	private Date date;
+	
+	public Activity(int id, String type, String description, Date date) {
+		this.id=id;
+		this.type=type;
+		this.description=description;
+		this.date=date;
+	}
+	
+    public int getId() {
+        return id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+}
