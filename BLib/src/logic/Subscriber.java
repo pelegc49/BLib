72143ce7a71d7a930f27f88cb5bea9cef767179d
lidorbd13 <Lidor.Ben.David@e.braude.ac.il>
2024-12-17 14:42:58 +0200

    added subscriber

---
+++ BLib/src/logic/Subscriber.java
@@ -0,0 +1,68 @@
+package logic;
+
+public class Subscriber {
+	
+	private int id;
+	private String name;
+	private String phone;
+	private String email;
+	private String userName;
+	private String status;
+	
+	public Subscriber(int id, String name, String phone, String email, String userName, String status) {
+		this.id=id;
+		this.name=name;
+		this.phone=phone;
+		this.email=email;
+		this.userName=userName;
+		this.status=status;
+	}
+	
+	public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
