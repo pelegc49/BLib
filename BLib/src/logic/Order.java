79381ee0a15a7f5d465b35c8b12945c66b7ca2bd
lidorbd13 <Lidor.Ben.David@e.braude.ac.il>
2024-12-17 15:02:20 +0200

    added order

---
+++ BLib/src/logic/Order.java
@@ -0,0 +1,18 @@
+package logic;
+
+public class Order {
+
+	private int id;
+	
+	public Order(int id) {
+		this.id=id;
+	}
+	
+	public void setId(int id) {
+		this.id=id;
+	}
+	
+	public int getId() {
+		return this.id;
+	}
+}
