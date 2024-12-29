43aa260ebebb1adc30c3d30c3d52e38f6e3e1551
Reuven Itzhakov <hitrifox@gmail.com>
2024-12-29 15:50:14 +0200

    clickable row

--- BLib/src/gui/client/SearchController.java
+++ BLib/src/gui/client/SearchController.java
@@ -1,18 +1,23 @@
 package gui.client;
 
+import java.net.URL;
+import java.util.ResourceBundle;
 import java.util.Set;
 
+import javafx.beans.property.ObjectProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableRow;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
@@ -20,6 +25,7 @@ import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+import javafx.util.Callback;
 import logic.BookTitle; 
 
 /**
@@ -27,7 +33,7 @@ import logic.BookTitle;
  * It manages the login process, including ID and password validation, 
  * and transitions the user to the main application interface upon successful login.
  */
-public class SearchController {
+public class SearchController implements Initializable{
 	
 	@FXML
 	private TextField txtSearch;
@@ -48,8 +54,8 @@ public class SearchController {
 	public void searchBtn(Event event) {
 		ObservableList<BookTitle> data;
 		String keyword = txtSearch.getText();
-		System.out.println("keyword = "+keyword);
 		Set<BookTitle> bookTitle = IPController.client.getTitlesByKeyword(keyword);
+		System.out.println("keyword = "+keyword);
 		System.out.println(bookTitle);
 		if(bookTitle == null) {
 			display("No result found");
@@ -59,52 +65,23 @@ public class SearchController {
 			for(BookTitle bt : bookTitle) {
 				data.add(bt);
 			}
-			authorColumn.setCellValueFactory(new PropertyValueFactory<>("Author"));
-			titleColumn.setCellValueFactory(new PropertyValueFactory<>("Title"));
+			authorColumn.setCellValueFactory(new PropertyValueFactory<>("authorName"));
+			titleColumn.setCellValueFactory(new PropertyValueFactory<>("titleName"));
 			bookTable.setItems(data);
+			bookTable.getSortOrder().add(authorColumn);
 		}
 		
-		
-//		String id; // String to store the entered ID.
-//		FXMLLoader loader = new FXMLLoader(); // FXMLLoader for loading the main GUI.
-//		int digit_id = 0; // Variable to hold the numeric value of the ID.
-//		id = txtId.getText(); // Retrieve the text entered in the ID field.
-//		String password = txtPassword.getText(); // Retrieve the text entered in the password field.
-//
-//		try {
-//			// Attempt to parse the ID to an integer.
-//			digit_id = Integer.parseInt(id);
-//		} catch (Exception e) {
-//			// Display an error message if the ID is not numeric.
-//			display("bad username - only digits");
-//		}
-//
-//		// Validate that the ID field is not empty.
-//		if (id.trim().isEmpty()) {
-//			display("You must enter an id number");
-//		} 
-//		// Validate that the password field is not empty.
-//		else if (txtPassword.getText().isEmpty()) {
-//			display("You must enter a password");
-//		} 
-//		// If both fields are valid, attempt to log in.
-//		else {
-//			switch(IPController.client.login(digit_id, password)) {
-//				case "subscriber":
-//					subscriber = IPController.client.getSubscriber(digit_id);
-//					nextPage(event, "SubscriberClientGUIFrame", "Subscriber Main Menu");
-//					break;
-//				case "librarian":
-//					nextPage(event, "LibrarianClientGUIFrame", "Librarian Main Menu");
-//					break;
-//				case "fail":
-//					display("ID or password are incorrect");
-//					break;
-//				// For debugging purposes. delete me later \/
-//				default:
-//					System.out.println("Developers! Something went wrong at login...");
-//			}
-//		}
+		// allows to click on row
+		bookTable.setRowFactory(tv -> {
+		    TableRow<BookTitle> rowa = new TableRow<>();
+		    rowa.setOnMouseClicked(eventa -> {
+		        if (eventa.getClickCount() == 2 && (! rowa.isEmpty()) ) {
+		        	BookTitle rowData = rowa.getItem();
+		            System.out.println(rowData);
+		        }
+		    });
+	    return rowa ;
+		});
 	}
 
 	/**
@@ -161,5 +138,11 @@ public class SearchController {
 		primaryStage.setTitle(title);
 		primaryStage.setScene(scene);
 		primaryStage.show();
+	}
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		
 	}
 }
