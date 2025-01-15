5de92d521fa46bb5c864a622bced6b6088a08afb
Eden654321 <Eden.Furman@e.braude.ac.il>
2025-01-15 22:45:20 +0200

    worked on the graphs

--- BLib/src/gui/client/ShowReportsController.java
+++ BLib/src/gui/client/ShowReportsController.java
@@ -1,7 +1,5 @@
 package gui.client;
 
-import javafx.beans.property.SimpleObjectProperty;
-import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -10,53 +8,50 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Scene;
-import javafx.scene.control.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
-import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
-import logic.Borrow;
-import logic.BorrowPlus;
-import logic.Message;
 import logic.Subscriber;
-
-import java.util.AbstractMap.SimpleEntry;
-import java.util.List;
-import java.util.Map.Entry;
 
 /**
  * The AuthenticationController class handles user authentication. 
  * It manages the login process, including ID and password validation, 
  * and transitions the user to the main application interface upon successful login.
  */
-public class ReportsController {
-	private Subscriber subscriber; // Static reference to the currently authenticated subscriber.
+public class ShowReportsController {
+	//private Subscriber subscriber; // Static reference to the currently authenticated subscriber.
 	@FXML
 	private Button btnBack = null; // Button to exit the application.
 	@FXML
-	private Button btnGenerateGraph = null; // Button to exit the application.
+	private Text txtName;
 	@FXML
-	private ChoiceBox<Integer> choiceBoxMonth;
+	private Text txtYear;
 	@FXML
-	private ChoiceBox<Integer> choiceBoxYear;
-	@FXML
-	private ChoiceBox<String> choiceBoxGraph;
+	private Text txtMonth;
 
 
-
-	public void loadChoiceBoxs() {
-		choiceBoxMonth.setValue(1);
-		choiceBoxYear.setValue(2024);
-		choiceBoxGraph.setValue("Borrowing Report");
-		ObservableList<Integer> dataMonth = FXCollections.observableArrayList();
-		for(Integer number = 1; number < 13; number++) {
-			dataMonth.add(number);
-		}
-		choiceBoxMonth.setItems(dataMonth);
-		choiceBoxYear.getItems().addAll(2024, 2025);
-		choiceBoxGraph.getItems().addAll("Borrowing Report", "Subscriber Status Report");
-
+	public void loadGraphDetails() {
+		txtName.setText("Borrowing Report");
+		txtYear.setText("2024");
+		txtMonth.setText("1");
 	}
+
+//	public void loadChoiceBoxs() {
+//		choiceBoxMonth.setValue(1);
+//		choiceBoxYear.setValue(2024);
+//		choiceBoxGraph.setValue("Borrowing Report");
+//		ObservableList<Integer> dataMonth = FXCollections.observableArrayList();
+//		for(Integer number = 1; number < 13; number++) {
+//			dataMonth.add(number);
+//		}
+//		choiceBoxMonth.setItems(dataMonth);
+//		choiceBoxYear.getItems().addAll(2024, 2025);
+//		choiceBoxGraph.getItems().addAll("Borrowing Report", "Subscriber Status Report");
+//
+//	}
 
 //	public void display(String message, Color color) {
 //		lblError.setTextFill(color); // Sets the color of the error label.
@@ -72,23 +67,20 @@ public class ReportsController {
 	 */
 	public void backBtn(ActionEvent event) throws Exception {
 		FXMLLoader loader = new FXMLLoader();
-		Pane root = loader.load(getClass().getResource("/gui/client/"+ "LibrarianClientGUIFrame" +".fxml").openStream());
-		LibrarianClientGUIController librarianClientGUIController = loader.getController();
-		librarianClientGUIController.updateMessageCount();
-		librarianClientGUIController.loadLibrarian();
-		nextPage(loader, root, event, "Librarian Main Menu");
-	}
-
-	public void GenerateGraphBtn(ActionEvent event) throws Exception {
-		FXMLLoader loader = new FXMLLoader();
-		Pane root = loader.load(getClass().getResource("/gui/client/"+ "ShowReportsFrame" +".fxml").openStream());
-		ShowReportsController showReportsController = loader.getController();
-//		showReportsController.loadChoiceBoxs();
-		nextPage(loader, root, event, "Show Reports");
+		Pane root = loader.load(getClass().getResource("/gui/client/"+ "ReportsFrame" +".fxml").openStream());
 //		LibrarianClientGUIController librarianClientGUIController = loader.getController();
 //		librarianClientGUIController.updateMessageCount();
 //		librarianClientGUIController.loadLibrarian();
+		nextPage(loader, root, event, "Choose Report");
+	}
 
+	public void GenerateGraphBtn(ActionEvent event) throws Exception {
+//		FXMLLoader loader = new FXMLLoader();
+//		Pane root = loader.load(getClass().getResource("/gui/client/"+ "LibrarianClientGUIFrame" +".fxml").openStream());
+//		LibrarianClientGUIController librarianClientGUIController = loader.getController();
+//		librarianClientGUIController.updateMessageCount();
+//		librarianClientGUIController.loadLibrarian();
+//		nextPage(loader, root, event, "Librarian Main Menu");
 	}
 
 	
